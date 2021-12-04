65f7e35ca5fcc660fbe47d72e45db21e26d92638
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-12-04 12:31:38 +0300

    Project ready

---
+++ src/main/java/players/club/CardGame.java
@@ -0,0 +1,8 @@
+package players.club;
+
+public class CardGame {
+    public static void main(String[] args) {
+        System.out.println("Hello, players!");
+        System.out.println(CardGame.class.getName());
+    }
+}
