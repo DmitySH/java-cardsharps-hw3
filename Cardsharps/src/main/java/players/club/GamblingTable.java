8f9a6af0b6fc78da7e61f6c20a07a6ad5b65c75d
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-12-06 11:18:03 +0300

    Project restructured

---
+++ Cardsharps/src/main/java/players/club/GamblingTable.java
@@ -0,0 +1,4 @@
+package players.club;
+
+public class GamblingTable {
+}
--- src/main/java/players/club/CardGame.java
+++
@@ -1,8 +0,0 @@
-package players.club;
-
-public class CardGame {
-    public static void main(String[] args) {
-        System.out.println("Hello, players!");
-        System.out.println(CardGame.class.getName());
-    }
-}
