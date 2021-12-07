52eec48b0ecf4351e47d6a2d99817d6a1b38c391
Дмитрий Шагаров <dashagarov@edu.hse.ru>
2021-12-07 23:31:12 +0300

    Game +- done

--- Cardsharps/src/main/java/players/club/blackjack/BlackjackTable.java
+++ Cardsharps/src/main/java/players/club/blackjack/BlackjackTable.java
@@ -2,41 +2,74 @@ package players.club.blackjack;
 
 import players.club.interfaces.Deck;
 import players.club.interfaces.GamblingTable;
+import players.club.interfaces.Input;
 import players.club.interfaces.Player;
 import players.club.players.Cardsharp;
 import players.club.players.Gambler;
+import players.club.utils.InputHelper;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class BlackjackTable implements GamblingTable {
-    private static final int GAME_TIME = 5_000; //todo: to 10sec
+    private static final int GAME_TIME = 3_000; //todo: to 10sec
+    private static final int MAX_INPUT_ATTEMPTS = 7;
+    private static final int MAX_EACH_TYPE_PLAYERS = 500;
 
     private final List<Player> players;
     private final Deck deck;
 
-    public static void main(String[] args) {
-        GamblingTable table = new BlackjackTable();
+    private static final Input inputHelper;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int gamblerCount;
+        int cardsharpCount;
+
+        //todo: remove
+        PrintStream out = new PrintStream(new FileOutputStream(
+                "C:\\Users\\dm1tr\\Desktop\\Python\\files\\java_out.txt"));
+        System.setOut(out);
+
+        try {
+            gamblerCount = inputHelper.parseInt(1, MAX_EACH_TYPE_PLAYERS,
+                    "Enter number of fair players: ",
+                    "Incorrect number! Try again: ", MAX_INPUT_ATTEMPTS);
+            cardsharpCount = inputHelper.parseInt(0, MAX_EACH_TYPE_PLAYERS,
+                    "Enter number of cardsharps: ",
+                    "Incorrect number! Try again: ", MAX_INPUT_ATTEMPTS);
+        } catch (NumberFormatException ex) {
+            System.out.println(ex.getMessage() + " Restart application!");
+            return;
+        }
+
+        GamblingTable table = new BlackjackTable(gamblerCount, cardsharpCount);
         table.playCardGame();
     }
 
+    static {
+        inputHelper = InputHelper.getInstance();
+    }
 
-    public BlackjackTable() {
+    public BlackjackTable(int gamblerCount, int cardsharpCount) {
         players = new ArrayList<>();
         deck = new BlackjackDeck();
 
-        for (int i = 0; i < 2; ++i) {
-            players.add(new Gambler(deck, "" + (i + 1)));
+        for (int i = 0; i < gamblerCount; ++i) {
+            players.add(new Gambler(deck, "Gambler_" + (i + 1)));
         }
 
-        for (int i = 0; i < 2; ++i) {
-            players.add(new Cardsharp(deck, "" + (i + 3)));
+        for (int i = 0; i < cardsharpCount; ++i) {
+            players.add(new Cardsharp(deck, "Cardsharp_" + (i + 1)));
         }
     }
 
     @Override
     public void playCardGame() {
         System.out.println("Blackjack started");
+
         try {
             playBlackjack();
         } catch (InterruptedException ex) {
@@ -45,16 +78,13 @@ public final class BlackjackTable implements GamblingTable {
 
         for (Player player : players) {
             System.out.println(player);
-            if (player instanceof Gambler gambler) {
-                System.out.println(gambler.got +"  " + gambler.lost);
-            }
         }
     }
 
     private void playBlackjack() throws InterruptedException {
         List<Thread> threads = new ArrayList<>();
         for (Player player : players) {
-            Thread thread = new Thread(player, "Player" + player.getName());
+            Thread thread = new Thread(player, player.getName());
             threads.add(thread);
             thread.start();
         }
